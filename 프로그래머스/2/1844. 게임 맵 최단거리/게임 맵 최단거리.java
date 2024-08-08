5810c0aa8632bbecc619b8a4145b1d9fd385ec2c
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-08 11:00:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.88 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,28 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int n = maps.length; // 행 수
-        int m = maps[0].length; // 열 수 
-        boolean[][] visited = new boolean[n][m]; // 방문 여부
-        Queue<int[]> queue = new ArrayDeque<>();
+        int m = maps[0].length; // 열 수
+        boolean[][] visited = new boolean[n][m]; // 방문 표시
+        Queue<int[]> queue = new ArrayDeque<>(); // r, c, dist
         
-        // bfs 초기화
-        queue.add(new int[]{0, 0, 1}); // 초기 위치(0,0), 초기 거리 1
-        visited[0][0] = true; // 방문 표시
+        // 상하좌우 거리벡터
+        int[] dr = {-1, 1, 0, 0}; // 행 변화량
+        int[] dc = {0, 0, -1, 1}; // 열 변화량
         
-        // 상하좌우 거리 벡터
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
+        // bfs 초기값
+        queue.add(new int[]{0, 0, 1}); // 시작 위치 (0,0), 거리 1 (자신 포함)
+        visited[0][0] = true; // 시작 위치 방문 표시
         
-        while(!queue.isEmpty()) {
-            int cur[] = queue.remove(); // 현재 위치와 거리
+        // bfs 탐색
+        while (!queue.isEmpty()) {
+            int[] cur = queue.remove();
             int r = cur[0], c = cur[1], dist = cur[2];
             
             // 목적지 도착
             if (r == n-1 && c == m-1) {
-                return dist;
+                return dist; // 거리 반환
             }
             
-            // 상하좌우 돌기
+            // 상하좌우로 이동하면서 탐색
             for (int d=0; d<4; d++) {
                 int nr = r + dr[d], nc = c + dc[d];
                 
@@ -35,5 +36,5 @@ class Solution {
             }
         }
         return -1;
-    }
+    } 
 }
