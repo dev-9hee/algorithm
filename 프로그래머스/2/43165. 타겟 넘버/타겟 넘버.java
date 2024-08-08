07a77382ec1cb65cc62db09c6988c55335867c15
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-08 23:15:47 +0900

    [level 2] Title: 타겟 넘버, Time: 7.22 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        // dfs 리턴
-        return dfs(numbers, 0, target, 0);
+        // bfs 리턴
+        return bfs(numbers, 0, target, 0);
     }
     
-    public int dfs(int[] numbers, int idx, int target, int cur) {
+    public int bfs(int[] numbers, int idx, int target, int cur) {
         // base-case
         if (idx == numbers.length) {
             return (cur == target) ? 1 : 0;
@@ -12,8 +12,8 @@ class Solution {
         
         // 재귀 호출
         int sum = 0;
-        sum += dfs(numbers, idx + 1, target, cur + numbers[idx]);
-        sum += dfs(numbers, idx + 1, target, cur - numbers[idx]);
+        sum += bfs(numbers, idx + 1, target, cur + numbers[idx]);
+        sum += bfs(numbers, idx + 1, target, cur - numbers[idx]);
         
         return sum;
     }
