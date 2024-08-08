87198900964da2615262ed8bfe6a496d834fcdb0
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-08 17:22:29 +0900

    [level 2] Title: 주식가격, Time: 26.13 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length]; // 가격이 떨어지지 않은 기간
-        Deque<Integer> stack = new ArrayDeque<>(); // 스택 사용
+        int[] answer = new int[prices.length];
+        Deque<Integer> stack = new ArrayDeque<>();
         
         // 주식 가격 순회
         for (int i=0; i<prices.length; i++) {
-            while (!stack.isEmpty()) { // 스택이 빌 때까지 반복
-                int j = stack.peek(); // 단순히 맨 위의 값 반환
+            while (!stack.isEmpty()) {
+                int j = stack.peek();
                 
                 if (prices[j] > prices[i]) {
                     answer[j] = i - j;
@@ -16,13 +16,13 @@ class Solution {
                 }
                 else break;
             }
-            stack.push(i); // 스택에 인덱스 i 값 추가
+            stack.push(i); // 인덱스 값 스택에 추가
         }
         
-        // 끝까지 가격이 떨어지지 않는 경우
+        // 끝까지 가격이 안 떨어짐
         while (!stack.isEmpty()) {
             int idx = stack.pop();
-            answer[idx] = prices.length - idx -1;
+            answer[idx] = prices.length - idx - 1;
         }
         
         return answer;
