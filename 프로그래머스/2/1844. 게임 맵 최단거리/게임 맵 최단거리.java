640c7906a4e6b5e877f43f7b9cb4155742f5472e
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-09 11:27:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.53 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -8,30 +8,30 @@ class Solution {
         Queue<int[]> queue = new ArrayDeque<>(); // r, c, dist
         
         // 상하좌우 거리벡터
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
+        int[] dr = {-1, 1, 0, 0}; // 행 변화량
+        int[] dc = {0, 0, -1, 1}; // 열 변화량
         
-        // dfs 초기화
-        queue.add(new int[]{0, 0, 1}); // 시작 위치 (0, 0), 시작 거리 1
+        // bfs 초기화
+        queue.add(new int[] {0, 0, 1}); // 시작위치 (0, 0), 현재 거리 1
         visited[0][0] = true; // 시작 위치 방문 표시
         
-        // dfs 탐색
+        // bfs 탐색 시작
         while (!queue.isEmpty()) {
             int[] cur = queue.remove();
             int r = cur[0], c = cur[1], dist = cur[2];
             
             // 목적지 도달
             if (r == n-1 && c == m-1) {
-                return dist;
+                return dist; // 거리 반환
             }
             
-            // 상하좌우 탐색
+            // 상하좌우 이동
             for (int d=0; d<4; d++) {
                 int nr = r + dr[d], nc = c + dc[d];
                 
                 if (0 <= nr && nr < n && 0 <= nc && nc < m && maps[nr][nc] == 1 && !visited[nr][nc]) {
                     visited[nr][nc] = true;
-                    queue.add(new int[]{nr, nc, dist + 1});
+                    queue.add(new int[] {nr, nc, dist + 1});
                 }
             }
         }
