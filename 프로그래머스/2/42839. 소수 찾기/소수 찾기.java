416d7a96b821f216aada838a88f336450d70b7ca
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-25 16:36:10 +0900

    [level 2] Title: 소수 찾기, Time: 5.87 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> candidates = new HashSet<>();
+    Set<Integer> candidates = new HashSet<>(); // 소수 후보들
     
     public int solution(String numbers) {
         // 숫자 조합
@@ -9,29 +9,30 @@ class Solution {
         // 소수 찾기
         int ans = 0;
         for (int num : candidates) {
-            if (isPrime(num)) ans++; // true면 소수이므로 증가
+            if (isPrime(num)) ans++; // isValid()가 true면 소수
         }
         return ans;
     }
     
+    // 숫자문자열, 현재수, 방문여부, 현재 자릿수
     void permutation(String numbers, int cur, boolean[] visited, int digit) {
         if (digit == numbers.length()) return;
         
         for (int i=0; i<numbers.length(); i++) {
-            if (visited[i]) continue;
+            if (visited[i]) continue; // 이미 방문
             
             int newValue = cur + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
-            candidates.add(newValue);
+            candidates.add(newValue); // 새로운 숫자 조합을 후보에 추가
             
             visited[i] = true;
             permutation(numbers, newValue, visited, digit + 1);
-            visited[i] = false; // 백트레킹을 위함
+            visited[i] = false;
         }
     }
     
     // 소수 판별
     boolean isPrime(int n) {
-        if(n < 2) return false;
+        if (n < 2) return false;
         
         for (int i=2; i*i<=n; i++) {
             if (n % i == 0) return false;
