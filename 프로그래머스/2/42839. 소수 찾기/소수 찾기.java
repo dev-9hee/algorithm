7afa43179af55b65c43672cdd1abc740bc5ef775
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-27 00:02:06 +0900

    [level 2] Title: 소수 찾기, Time: 5.44 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -9,20 +9,20 @@ class Solution {
         // 소수 찾기
         int ans = 0;
         for (int num : candidates) {
-            if (isPrime(num)) ans++; // isValid()가 true면 소수
+            if (isPrime(num)) ans++; // 소수면 개수 증가
         }
         return ans;
     }
     
-    // 숫자문자열, 현재수, 방문여부, 현재 자릿수
+    // cur : 현재 수, digit : 현재 자리 수
     void permutation(String numbers, int cur, boolean[] visited, int digit) {
         if (digit == numbers.length()) return;
         
         for (int i=0; i<numbers.length(); i++) {
-            if (visited[i]) continue; // 이미 방문
+            if (visited[i]) continue; // 이미 방문했다면 패스!
             
             int newValue = cur + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
-            candidates.add(newValue); // 새로운 숫자 조합을 후보에 추가
+            candidates.add(newValue);
             
             visited[i] = true;
             permutation(numbers, newValue, visited, digit + 1);
@@ -30,7 +30,6 @@ class Solution {
         }
     }
     
-    // 소수 판별
     boolean isPrime(int n) {
         if (n < 2) return false;
         
