a0e61a57ca533577ab5c95767054198b8aaccd12
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-28 20:33:04 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] computers) {
-        int count = 0; // 네트워크 개수
-        boolean[] visited = new boolean[n]; // 방문 표시
+        int count = 0; // 네트워크 수
+        boolean[] visited = new boolean[n];
         
         for (int i=0; i<n; i++) {
-            if (visited[i]) continue; // 이미 방문했다면 뛰어 넘기
+            if (visited[i]) continue; // 이미 방문, 패스!!
             dfs(n, computers, visited, i);
             count++; // 네트워크 수 증가
         }
         return count;
     }
     
-    // dfs 구현
-    void dfs(int n, int[][] computers, boolean[] visited, int cur) { // cur : 현재 컴퓨터
-        visited[cur] = true; // 현재 컴퓨터 방문 표시
+    // cur : 현재 방문 컴퓨터
+    void dfs(int n, int[][] computers, boolean[] visited, int cur) {
+        visited[cur] = true; // 방문 처리
         
         for (int i=0; i<n; i++) {
-            // 방문하지 않았고 현재 컴퓨터와 연결되어 있다면
+            // 아직 방문하지 않았고 현재 컴퓨터와 연결되어 있다면
             if (!visited[i] && computers[cur][i] == 1) {
                 dfs(n, computers, visited, i);
             }
         }
-        
     }
 }
